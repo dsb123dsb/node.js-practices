1d122092b74c538337a279376b1de8ffa4048807
zhouyonghui <zyh@treeholeapp.cn>
2017-02-26 22:45:10 +0800

    http server-req.header

---
+++ http-server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	console.log(req.headers); //打印其余报头
+	const buffers = [];
+	req.on('data', (trunk) => {
+		buffers.push(trunk);
+	}).on('end', () => {
+		const buffer = Buffer.concat(buffers);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Hello World\n');
+	});
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
