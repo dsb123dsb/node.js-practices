ddf06abcf45604ed1e1ac0131ec0c966045394eb
zhouyonghui <523598438@qq.com>
2018-01-04 09:45:47 +0800

    response head

--- http server-client/http-server.js
+++ http server-client/http-server.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 	}).on('end', () => {
 		const buffer = Buffer.concat(buffers);
 		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/plain');
+		res.writeHead('Content-Type', 'text/plain');
 		res.end('Hello World\n');
 	});
 });
