d8d237083fb4008b1bccc9ec37b58b3b2f4e4e27
dsb123dsb <523598438@qq.com>
2018-01-06 21:18:49 +0800

    child_process1

---
+++ child_process/work.js
@@ -0,0 +1,7 @@
+const http = require('http');
+let port = Math.round((1+Math.random())*1000);
+http.createServer((req, res)=>{
+	res.writeHead(200, {'Content-Type': 'Text/plain'});
+	res.end('Hello world\n');
+}).listen(port, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:'+port);
