121f9f68ad65deeaa4e2025c56b2766246c0fcb1
dsb123dsb <523598438@qq.com>
2018-01-08 23:01:18 +0800

    process自动重启

---
+++ child_process/IPC_master.js
@@ -0,0 +1,44 @@
+let cpus = require('os').cpus();
+// 绝对路径
+let fork = require('child_process').fork;
+let server = require('net').createServer();
+
+server.on('connection',(socket)=>{
+	socket.end('handled by parent\n');
+});
+server.listen(1337, ()=>{
+	// server.close(); //关闭tcp时，杀死子进程会导致不断重启 
+})
+
+// 复制进程并发送句柄,设置自动重启
+let workers = {}; // 存放进程
+let createWorker = ()=>{
+	let worker = fork(__dirname+'/IPC_worker.js');
+	// 退出时重启新的进程
+	worker.on('exit', ()=>{
+		console.log('Worker '+ worker.pid + ' exited');
+		delete workers[worker.pid];
+		createWorker();
+	});
+	// 句柄转发
+	worker.send('server', server);
+	workers[worker.pid]=worker;
+	console.log('Create worker. pid: '+worker.pid);
+};
+
+for(let i = 0; i<cpus.length; i++){
+	createWorker();
+}
+
+// 主进程自己退出时，让所有工作进程退出
+process.on('exit', ()=>{
+	for(let pid in workers){
+		workersp[pid].kill()
+	}
+})
+// n.kill('SIGTERM'); // 杀死子进程
+
+// n.on('message', (m)=>{
+// 	console.log('PARENT got message:',m);
+// });
+// n.send({hello:'world'});
--- child_process/IPC_worker.js
+++ child_process/IPC_worker.js
@@ -1,12 +1,14 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{ // 转化http层
+let http = require('http');
+let server = http.createServer((req,res)=>{ // 转化http层
 	res.writeHead(200, {'Content-type': 'text/plain'});
 	res.end('handled by child, pid is ' + process.pid+'\n');
 });
+let worker;
 process.on('message',(m,tcp)=>{
 	console.log('CHILD got message:',m);
 	if(m='server'){
-		tcp.on('connection', (socket)=>{
+		worker=tcp;
+		worker.on('connection', (socket)=>{
 			// console.log('where I am ?') // 这里打印无意义
 			// socket.end('handled by tcp, pid is '+ process.pid+'\n');
 			server.emit('connection',socket); // 接受句柄同时tcp连接时，http触发connection事件
@@ -14,4 +16,16 @@ process.on('message',(m,tcp)=>{
 		});
 	}
 });
+process.on('uncaughtException', ()=>{
+	//停止接收新的连接
+	worker.close(()=>{
+		//断开所有连接
+		process.exit(1);
+	});
+})
+// process.on('SIMTERM', ()=>{
+// 	console.log('Got a SIGTERM, exiting...');
+// 	process.exit(1);
+// })
+// process.kill(process.pid, 'SIGTERM'); // 杀死子进程
 // process.send({foo:'bar'});
